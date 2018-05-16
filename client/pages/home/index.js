ff6afc674694312d0742065ba735b4ec84ad68fc
Your Name <you@example.com>
2018-05-16 18:28:49 +0800

    增加了首页UI

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -5,9 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    bannerList: [
+      '../../image/banner/banner1.jpg',
+      '../../image/banner/banner2.jpg'
+    ],
+    containerDemo: [],
+    animationStatus: false
   },
-
+  loop () {
+    let BannerBox = [
+      '../../image/banner/banner3.png',
+      '../../image/banner/banner4.jpg',
+      '../../image/banner/banner5.jpg'
+    ]
+    let doloop = () => {
+      setTimeout(() => {
+        let piece = BannerBox.shift()
+        BannerBox.push(piece)
+        // BannerBox.unshift()
+        this.setData({
+          animationStatus: false,
+          containerDemo: [
+            {
+              url1: BannerBox[0],
+              url2: BannerBox[0],
+            },
+            {
+              url1: BannerBox[1],
+              url2: BannerBox[1],
+            },
+            {
+              url1: BannerBox[2],
+              url2: BannerBox[2],
+            }
+          ]
+        })
+        this.setData({
+          containerDemo: [
+            {
+              url1: BannerBox[0],
+              url2: BannerBox[1],
+            },
+            {
+              url1: BannerBox[1],
+              url2: BannerBox[2],
+            },
+            {
+              url1: BannerBox[2],
+              url2: BannerBox[0],
+            }
+          ]
+        })
+        setTimeout(() => {
+          this.setData({
+            animationStatus: true
+          })
+          doloop()
+        }, 100)
+      }, 2000)
+    }
+    doloop()
+    this.setData({
+      animationStatus: true
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,7 +80,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.loop()
   },
 
   /**
@@ -40,7 +101,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
